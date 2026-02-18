966872a65054c827f0d54eb461049ee8df28f8fc
ayushi raina <ayushiraina02@gmail.com>
2026-02-19 00:53:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+faq = {
+    "hi": "Hello! How can I help you?",
+    "return policy": "Our return policy is valid for 7 days.",
+    "track order": "You can track your order from your account section.",
+    "payment methods": "We accept UPI, Debit Card, Credit Card and Net Banking."
+}
+
+@app.route("/")
+def home():
+    return "AI Customer Support Chatbot is running!"
+
+@app.route("/get", methods=["POST"])
+def chatbot():
+    user_input = request.form["msg"].lower()
+    response = "Sorry, I didn't understand that."
+    
+    for key in faq:
+        if key in user_input:
+            response = faq[key]
+    
+    return response
+
+if __name__ == "__main__":
+    app.run()
